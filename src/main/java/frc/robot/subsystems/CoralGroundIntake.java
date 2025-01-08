ad45ea8826d22a48badf8bc163849c386451f87e
DriverStation3 <194180343+RobototesProgrammers@users.noreply.github.com>
2025-01-07 18:22:52 -0800

    Added the two prototyped subsystems
    Added algae ground intake subsystem
    
    Added coral ground intake subsystem
    
    Bound controls for both

---
+++ src/main/java/frc/robot/subsystems/CoralGroundIntake.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems;
+
+import java.util.function.DoubleSupplier;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class CoralGroundIntake extends SubsystemBase {
+    private final TalonFX topMotor;
+    private final TalonFX bottomMotor;
+
+    public CoralGroundIntake() {
+        topMotor = new TalonFX(32);
+        bottomMotor = new TalonFX(31);
+    }
+
+    public Command SpinSpeed(DoubleSupplier speed) {
+        return run(() -> {
+            topMotor.set(speed.getAsDouble());
+            bottomMotor.set(speed.getAsDouble());
+        });
+    }
+}
